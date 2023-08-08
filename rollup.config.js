683a484d155e5708872961b0132a0b77c5e9bba0
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2023-08-08 13:02:25 -0400

    add custom loc

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,28 @@
-  
 import typescript from '@rollup/plugin-typescript';
-import {nodeResolve} from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import dotenv from 'dotenv';
+import copy from 'rollup-plugin-copy';
+
+dotenv.config();
+
+const rootPath = `${process.env.OBSIDIAN_PATH}/consistent-attachments-and-links`;
 
 export default {
   input: 'src/main.ts',
   output: {
-    dir: '.',
+    dir: rootPath,
     sourcemap: 'inline',
     format: 'cjs',
-    exports: 'default'
+    exports: 'default',
   },
   external: ['obsidian'],
   plugins: [
     typescript(),
-    nodeResolve({browser: true}),
+    nodeResolve({ browser: true }),
     commonjs(),
-  ]
+    copy({
+      targets: [{ src: 'manifest.json', dest: rootPath }],
+    }),
+  ],
 };
